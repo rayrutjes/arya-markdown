4b750a43bf3709c9a023ee4244ea2b79d1c5675f
Raymond Rutjes <raymond.rutjes@gmail.com>
2016-07-11 22:28:54 +0200

    Inject parsedown as a dependency of the plugin

--- src/AryaMarkdown.php
+++ src/AryaMarkdown.php
@@ -2,11 +2,33 @@
 
 namespace RayRutjes\AryaMarkdown;
 
+use Parsedown;
 use RayRutjes\Arya\Arya;
 use RayRutjes\Arya\Plugin;
 
 class AryaMarkdown implements Plugin
 {
+    /**
+     * @var Parsedown
+     */
+    private $parser;
+
+    /**
+     * @param Parsedown $parser
+     */
+    public function __construct(Parsedown $parser = null)
+    {
+        $this->parser = $parser ?? new Parsedown();
+    }
+
+    /**
+     * @return Parsedown
+     */
+    public function getParser()
+    {
+        return $this->parser;
+    }
+
     /**
      * @param array $files
      * @param Arya  $arya
--- tests/AryaMarkdownTest.php
+++ tests/AryaMarkdownTest.php
@@ -2,6 +2,21 @@
 
 namespace RayRutjes\AryaMarkdown\Test;
 
+use Parsedown;
+use RayRutjes\AryaMarkdown\AryaMarkdown;
+
 class AryaMarkdownTest extends \PHPUnit_Framework_TestCase
 {
+    public function testCanBeInitializedWithAParsedownInstance()
+    {
+        $parser = new Parsedown();
+        $plugin = new AryaMarkdown($parser);
+        $this->assertSame($parser, $plugin->getParser());
+    }
+
+    public function testCanLazyLoadADefaultParsedownInstance()
+    {
+        $plugin = new AryaMarkdown();
+        $this->assertInstanceOf(Parsedown::class, $plugin->getParser());
+    }
 }
